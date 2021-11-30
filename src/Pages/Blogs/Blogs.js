703a2585b9c1330847529ceb042d82c9b83fd2c2
mijanrony12e <mijaninfo12@gmail.comm>
2021-11-30 15:47:06 +0600

    refinish

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -4,7 +4,7 @@ import Header from '../Homes/Header/Header';
 const Blogs = () => {
     return (
         <div>
-            <Header/>
+            
               <h1 style={{color:'white',textAlign:'center', marginTop:'100px'}}>Blog Coming Soon.....</h1>
         </div>
     );
