c2d5782bfd93f8b6abaec311e46610e79e6835c4
mijanrony12e <mijaninfo12@gmail.comm>
2021-11-30 17:01:11 +0600

    refinish

--- src/App.js
+++ src/App.js
@@ -9,11 +9,26 @@ import About from './Pages/Homes/About/About';
 import Details from './Pages/Homes/Details/Details';
 import Header from './Pages/Homes/Header/Header';
 import Footer from './Pages/Footer/Footer';
+import { useEffect, useState } from 'react';
+import ClipLoader from "react-spinners/BounceLoader";
 
 function App() {
+  const [ loading, setLoading ] = useState(false)
+  
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+        setLoading(false)
+    }, 5000)
+  }, [])
+  
   return (
-    <div >
-      <BrowserRouter>
+    <div className="">
+      {
+        loading ?
+          <div className="spin-loader"><ClipLoader color={"#e67e22"} loading={loading}  size={100} /></div>
+          :
+           <BrowserRouter>
            <Header/>
            <Routes>
                    <Route path="/" element={<Home/>}/>
@@ -23,9 +38,11 @@ function App() {
                    <Route path=":id" element={<Details/>}/>
                    <Route path="contact" element={<Contact/>}/>
             </Routes>
-             
+              <Footer></Footer>
       </BrowserRouter>
-            <Footer></Footer>
+            
+      }
+      
     </div>
   );
 }
